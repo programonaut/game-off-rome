2b91aedef4dbd622535f1e61d59004340750729f
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2022-11-18 08:02:12 +0100

    feat: suspicousness system
    * chore: cleanup
    
    * feat: suspicousness system

---
+++ Assets/Scripts/Supsicousness/SupicousnessSystem.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class SupicousnessSystem : MonoBehaviour
+{
+    public static SupicousnessSystem Instance { get; private set; }
+
+    [SerializeField, Range(0,100)] private int suspicousness = 0;
+    public int Suspicousness { get => suspicousness; }
+
+    private int suspiciosnessFrame = 30;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void IncreaseSuspicousness(int amount)
+    {
+        suspicousness += amount;
+        if (suspicousness > 100)
+        {
+            suspicousness = 100;
+        }
+    }
+
+    public void DecreaseSuspicousness(int amount)
+    {
+        suspicousness -= amount;
+        if (suspicousness < 0)
+        {
+            suspicousness = 0;
+        }
+    }
+
+    public void Caught() 
+    {
+
+    }
+
+    public int GetCatchPercentage() 
+    {
+        int min = suspicousness - suspiciosnessFrame > 0 ? suspicousness - suspiciosnessFrame : 0;
+        int max = suspicousness;
+
+        if (suspicousness == 100) min = 100;
+
+        return Random.Range(min, max + 1);
+    }
+}
