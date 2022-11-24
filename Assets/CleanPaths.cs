cca7c37d89e03254cfab9cb7229a791f488ae398
programonaut <dev@maximilian-kuerschner.de>
2022-11-24 10:58:03 +0100

    feat: nearly finish streets

---
+++ Assets/CleanPaths.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class CleanPaths : MonoBehaviour
+{
+    void KeepChildAndDeletePath() {
+
+    }
+
+    void MoveChildToParentAndDeactivate() {
+
+    }
+}
