763bc2fbabb6a7877d672d002e02e9815e982180
programonaut <dev@maximilian-kuerschner.de>
2022-11-24 11:05:49 +0100

    chore: activate and deactivate path creator on all paths (against lag)

--- Assets/CleanPaths.cs
+++ Assets/CleanPaths.cs
@@ -2,14 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using PathCreation;
 
 public class CleanPaths : MonoBehaviour
 {
+    [Button]
     void KeepChildAndDeletePath() {
 
     }
 
+    [Button]
     void MoveChildToParentAndDeactivate() {
+        PathCreator[] paths = FindObjectsOfType<PathCreator>();
+        foreach (PathCreator path in paths) {
+            if (path.transform.childCount > 0) {
+                path.transform.GetChild(0).SetParent(path.transform.parent);
+            }
+            path.gameObject.SetActive(false);
+        }
+    }
 
+    [Button]
+    void ReactivateAllPaths() {
+        PathCreator[] paths = FindObjectsOfType<PathCreator>(true);
+        foreach (PathCreator path in paths) {
+            path.gameObject.SetActive(true);
+        }
     }
 }
